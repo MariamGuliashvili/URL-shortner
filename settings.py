551a0fc1a206af294b108f558aa3b46529892194
Mariam guliashvili <146021619+MariamGuliashvili@users.noreply.github.com>
2024-01-18 23:38:27 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,8 +5,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-hsopux-y8@)(18bk6d#!1-$-r%#jz93jtn7*6_36&(pn3r)%4h'
